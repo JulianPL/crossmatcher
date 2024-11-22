b07b7c64b17dcbdb2b629411e5acacdd2beb890d
Julian Pape-Lange <j.pape-lange@gmx.de>
2024-11-22 13:07:06 +0100

    :sparkles: Add merging of linear candidates

--- lin/candidate.go
+++ lin/candidate.go
@@ -41,10 +41,24 @@ func MakeCandidateFromString(contentString string) Candidate {
 	return Candidate{content, alphabet}
 }
 
-func (candidate Candidate) IncrementCandidate() (Candidate, bool) {
+func (c Candidate) Len() int {
+	return len(c.Content)
+}
+
+func (c Candidate) CountWildcards() int {
+	count := 0
+	for _, char := range c.Content {
+		if char == -1 {
+			count++
+		}
+	}
+	return count
+}
+
+func (c Candidate) IncrementCandidate() (Candidate, bool) {
 	success := false
-	increment := Candidate{candidate.Content.Copy(), candidate.Alphabet}
-	for i := 0; i < len(candidate.Content); i++ {
+	increment := Candidate{c.Content.Copy(), c.Alphabet}
+	for i := 0; i < len(c.Content); i++ {
 		if increment.Content[i] < increment.Alphabet.Len()-1 {
 			increment.Content[i] += 1
 			success = true
@@ -55,22 +69,48 @@ func (candidate Candidate) IncrementCandidate() (Candidate, bool) {
 	return increment, success
 }
 
-func (content Content) Copy() Content {
+func (c Content) Copy() Content {
 	var newContent Content
-	for _, char := range content {
+	for _, char := range c {
 		newContent = append(newContent, char)
 	}
 	return newContent
 }
 
-func (candidate Candidate) GetRow() (string, bool) {
+func (c Candidate) GetRow() (string, bool) {
 	rowString := ""
-	for i := 0; i < len(candidate.Content); i++ {
-		char, ok := candidate.Alphabet.Char(candidate.Content[i])
+	for i := 0; i < len(c.Content); i++ {
+		char, ok := c.Alphabet.Char(c.Content[i])
 		if !ok {
 			return "", false
 		}
 		rowString += string(char)
 	}
 	return rowString, true
+}
+
+func (c Candidate) MergeRow(cFill Candidate) (string, bool) {
+	if c.CountWildcards() != cFill.Len() {
+		return "", false
+	}
+	rowFill, ok := cFill.GetRow()
+	if !ok {
+		return "", false
+	}
+	runesFill := []rune(rowFill)
+	currentFill := 0
+	var runesMerge []rune
+	for _, num := range c.Content {
+		if num != -1 {
+			char, ok := c.Alphabet.Char(num)
+			if !ok {
+				return "", false
+			}
+			runesMerge = append(runesMerge, char)
+		} else {
+			runesMerge = append(runesMerge, runesFill[currentFill])
+			currentFill++
+		}
+	}
+	return string(runesMerge), true
 }
--- lin/candidate_test.go
+++ lin/candidate_test.go
@@ -59,6 +59,24 @@ func TestCandidate_MakeCandidateFromString(t *testing.T) {
 	}
 }
 
+func TestCandidate_Len(t *testing.T) {
+	alphabet := collection.MakeAlphabet("a")
+	numA, _ := alphabet.Number('a')
+	candidate := MakeCandidate([]int{numA, -1, numA, -1, -1}, alphabet)
+	if candidate.Len() != 5 {
+		t.Errorf("Len is incorrect expected %d, actual %d", 5, candidate.Len())
+	}
+}
+
+func TestCandidate_CountWildcards(t *testing.T) {
+	alphabet := collection.MakeAlphabet("a")
+	numA, _ := alphabet.Number('a')
+	candidate := MakeCandidate([]int{numA, -1, numA, -1, -1}, alphabet)
+	if candidate.CountWildcards() != 3 {
+		t.Errorf("CountWildcards is incorrect expected %d, actual %d", 3, candidate.Len())
+	}
+}
+
 func TestCandidate_IncrementCandidate(t *testing.T) {
 	alphabet := collection.MakeAlphabet("0€1")
 	candidate := MakeCandidateFirst(alphabet, 6)
@@ -70,5 +88,38 @@ func TestCandidate_IncrementCandidate(t *testing.T) {
 	}
 	if count != 729 {
 		t.Errorf("Incorrect number of distinct candidates: expected 729, got %d", count)
+	}
+}
+
+func TestCandidate_MergeRow(t *testing.T) {
+	alphabet := collection.MakeAlphabet("ab")
+	numA, _ := alphabet.Number('a')
+	numB, _ := alphabet.Number('b')
+	candidate1 := MakeCandidate([]int{numA, numA, -1, -1, numA}, alphabet)
+	candidate2 := MakeCandidate([]int{numB, numB}, alphabet)
+	merge, ok := candidate1.MergeRow(candidate2)
+	if !ok {
+		t.Errorf("MergeRow is incorrect, expected success")
+	}
+	if merge != "aabba" {
+		t.Errorf("MergeRow is incorrect, expected \"aabba\", got %s", merge)
+	}
+	candidate1 = MakeCandidate([]int{numA, numA, -1, -1, numA}, alphabet)
+	candidate2 = MakeCandidate([]int{numB}, alphabet)
+	merge, ok = candidate1.MergeRow(candidate2)
+	if ok {
+		t.Errorf("MergeRow is incorrect, merged rows with filler of wrong size")
+	}
+	candidate1 = MakeCandidate([]int{numA, numA, -1, -1, numA}, alphabet)
+	candidate2 = MakeCandidate([]int{numB, -1}, alphabet)
+	merge, ok = candidate1.MergeRow(candidate2)
+	if ok {
+		t.Errorf("MergeRow is incorrect, merged rows with wildcards")
+	}
+	candidate1 = MakeCandidate([]int{numA, numA, -1, 10, numA}, alphabet)
+	candidate2 = MakeCandidate([]int{numB}, alphabet)
+	merge, ok = candidate1.MergeRow(candidate2)
+	if ok {
+		t.Errorf("MergeRow is incorrect, merged rows with undefined charactes")
 	}
 }
